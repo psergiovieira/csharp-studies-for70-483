de6ce8ce8968a3ba993d35b2855c9f0986742ee7
psergiovieira <psvieira.ti@gmail.com>
2018-12-26 21:31:05 -0200

    starting a test project

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Threading;
+
+namespace myFirstApp
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            TestingThreads();
+        }
+
+        public static void TestingThreads()
+        {
+            Thread t = new Thread(new ThreadStart(ThreadMethod));
+            t.Start();
+
+            for (int i = 0; i < 4; i++)
+            {
+                Console.WriteLine("Main thread: Do some work");
+                Thread.Sleep(0);
+            }
+
+            t.Join();
+        }
+
+        public static void ThreadMethod()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("ThreadProc: {0}", i);
+                Thread.Sleep(0);
+            }
+        }
+    }
+}
