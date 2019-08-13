99efc92b9141d1a431bb143fbce120a2d0139c27
psergiovieira <psvieira.ti@gmail.com>
2019-08-13 19:49:02 -0300

    Testing driverinfo on .netcore

--- Program.cs
+++ Program.cs
@@ -1,7 +1,5 @@
 ﻿using System;
-using System.Net.Http;
-using System.Threading;
-using System.Threading.Tasks;
+using System.IO;
 
 namespace myFirstApp
 {
@@ -9,14 +7,32 @@ namespace myFirstApp
     {
         public static void Main(string[] args)
         {
-
+            DriverTest();
             Console.ReadKey();
         }
 
         private static void DynamicTest()
         {
             dynamic obj = new SampleObject();
-            Console.WriteLine(obj.SomeProperty);
+            Console.WriteLine(obj.SameProperty);
+        }
+
+        private static void DriverTest()
+        {
+            DriveInfo[] drivesInfo = DriveInfo.GetDrives();
+            foreach (DriveInfo driveInfo in drivesInfo)
+            {
+                Console.WriteLine("Drive {0}", driveInfo.Name);
+                Console.WriteLine(" File type: {0}", driveInfo.DriveType);
+                if (driveInfo.IsReady == true)
+                {
+                    Console.WriteLine(" Volume label: {0}", driveInfo.VolumeLabel);
+                    Console.WriteLine(" File system: {0}", driveInfo.DriveFormat);
+                    Console.WriteLine(" Available space to current user:{0} bytes", driveInfo.AvailableFreeSpace);
+                    Console.WriteLine("Total available space: {0} bytes", driveInfo.TotalFreeSpace);
+                    Console.WriteLine(" Total size of drive:  {0} bytes", driveInfo.TotalSize);
+                }
+            }
         }
     }
 }
--- SampleObject.cs
+++ SampleObject.cs
@@ -4,6 +4,12 @@ namespace myFirstApp
 {
     public class SampleObject : DynamicObject
     {
+        public string MyProperty { get; set; }
+        public SampleObject()
+        {
+            MyProperty = "teste";
+        }
+
         public override bool TryGetMember(GetMemberBinder binder, out object result)
         {
             result = binder.Name;
